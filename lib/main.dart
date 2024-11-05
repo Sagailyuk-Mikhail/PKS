c4743edb28fe0e4ad4950dca864729d81e652d25
Sagailyuk-Mikhail <146726017+Sagailyuk-Mikhail@users.noreply.github.com>
2024-11-05 15:19:48 +0300

    Update main.dart
    ...

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter ПКС5_3',
+      title: 'Сагайлюк Михаил Александрович ЭФБО-03-22',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
